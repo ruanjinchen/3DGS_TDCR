d01a6491d7d9df06d334897c91e688d40595df6f
hukej <hukejun5@outlook.com>
2024-03-04 13:41:25 +0800

    添加了数据集读取关节角度

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -10,6 +10,8 @@
 #
 
 import torch
+from PIL import Image
+import numpy as np
 
 def mse(img1, img2):
     return (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
@@ -17,3 +19,15 @@ def mse(img1, img2):
 def psnr(img1, img2):
     mse = (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
     return 20 * torch.log10(1.0 / torch.sqrt(mse))
+
+def load_image(image_path, white_background=False):
+    image = Image.open(image_path)
+
+    im_data = np.array(image.convert("RGBA"))
+
+    bg = np.array([1, 1, 1]) if white_background else np.array([0, 0, 0])
+
+    norm_data = im_data / 255.0
+    arr = norm_data[:, :, :3] * norm_data[:, :, 3:4] + bg * (1 - norm_data[:, :, 3:4])
+    image = Image.fromarray(np.array(arr * 255.0, dtype=np.byte), "RGB")
+    return image
