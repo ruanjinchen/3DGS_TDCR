987f63f97dcf5fb5e2eef24e265c886c5c5405db
ruanjinchen <ruanjinchen@foxmail.com>
2026-01-19 22:49:22 +0800

    更新所有训练指令

--- all_command.py
+++ all_command.py
@@ -13,7 +13,7 @@ python train2.py \
   --joints 6 \
   --lambda_mask 2.0 \
   --opacity_reset_interval 100000000 \
-  -u 7000
+  -u 7000 --port 7001
 
 检查第一阶段效果
 export CUDA_VISIBLE_DEVICES=5
@@ -26,7 +26,7 @@ python render.py \
 cp out_tdcr2_no_base/chkpnt_7000.pth out_tdcr2_no_base/chkpnt_7000_stage1_backup.pth
 
 第二阶段 学习形变
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=4
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/2m_no_base.all \
   -m out_tdcr2_no_base_stage2 \
@@ -35,7 +35,7 @@ python train2.py \
   --lambda_mask 2.0 \
   --lambda_dssim 0 \
   -u 7000 \
-  --iterations 30000
+  --iterations 30000 --port 7002
  
 检查第二阶段效果
 export CUDA_VISIBLE_DEVICES=0
@@ -56,14 +56,14 @@ python render.py \
 -------------------------------------------------------------------------------------------------------------------------
 
 第一阶段训练 训练3D高斯 无形变
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=1
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/2m_with_base.zero \
   -m out_tdcr2_with_base \
   --joints 6 \
   --lambda_mask 2.0 \
   --opacity_reset_interval 100000000 \
-  -u 7000
+  -u 7000 --port 7003
 
 
 检查第一阶段效果
@@ -76,7 +76,7 @@ python render.py \
 cp out_tdcr2_with_base/chkpnt_7000.pth out_tdcr2_with_base/chkpnt_7000_stage1_backup.pth
 
 第二阶段 学习形变
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=5
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/2m_with_base.all \
   -m out_tdcr2_with_base_stage2 \
@@ -85,7 +85,7 @@ python train2.py \
   --lambda_mask 2.0 \
   --lambda_dssim 0 \
   -u 7000 \
-  --iterations 30000
+  --iterations 30000 --port 7004
  
 检查第二阶段效果
 export CUDA_VISIBLE_DEVICES=5
@@ -103,14 +103,14 @@ python render.py \
 -------------------------------------------------------------------------------------------------------------------------
 
 第一阶段训练 训练3D高斯 无形变
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=4
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/3m_no_base.zero \
   -m out_tdcr3_no_base \
   --joints 9 \
   --lambda_mask 2.0 \
   --opacity_reset_interval 100000000 \
-  -u 7000
+  -u 7000 --port 7005
 
 
 检查第一阶段效果
@@ -123,7 +123,7 @@ python render.py \
 cp out_tdcr3_no_base/chkpnt_7000.pth out_tdcr3_no_base/chkpnt_7000_stage1_backup.pth
 
 第二阶段 学习形变
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=5
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/3m_no_base.all \
   -m out_tdcr3_no_base_stage2 \
@@ -132,7 +132,7 @@ python train2.py \
   --lambda_mask 2.0 \
   --lambda_dssim 0 \
   -u 7000 \
-  --iterations 30000
+  --iterations 30000 --port 7006
  
 检查第二阶段效果
 export CUDA_VISIBLE_DEVICES=0
@@ -150,14 +150,14 @@ python render.py \
 -------------------------------------------------------------------------------------------------------------------------
 
 第一阶段训练 训练3D高斯 无形变
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=5
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/3m_with_base.zero \
   -m out_tdcr3_with_base \
   --joints 9 \
   --lambda_mask 2.0 \
   --opacity_reset_interval 100000000 \
-  -u 7000
+  -u 7000 --port 7007
 
 
 检查第一阶段效果
@@ -170,7 +170,7 @@ python render.py \
 cp out_tdcr3_with_base/chkpnt_7000.pth out_tdcr3_with_base/chkpnt_7000_stage1_backup.pth
 
 第二阶段 学习形变
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=3
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/3m_with_base.all \
   -m out_tdcr3_with_base_stage2 \
@@ -179,7 +179,7 @@ python train2.py \
   --lambda_mask 2.0 \
   --lambda_dssim 0 \
   -u 7000 \
-  --iterations 30000
+  --iterations 30000 --port 7008
  
 检查第二阶段效果
 export CUDA_VISIBLE_DEVICES=0
@@ -204,7 +204,7 @@ python train2.py \
   --joints 15 \
   --lambda_mask 2.0 \
   --opacity_reset_interval 100000000 \
-  -u 7000
+  -u 7000 --port 7009
 
 
 检查第一阶段效果
@@ -217,7 +217,7 @@ python render.py \
 cp out_tdcr5_no_base/chkpnt_7000.pth out_tdcr5_no_base/chkpnt_7000_stage1_backup.pth
 
 第二阶段 学习形变
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=4
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/5m_no_base.all \
   -m out_tdcr5_no_base_stage2 \
@@ -226,7 +226,7 @@ python train2.py \
   --lambda_mask 2.0 \
   --lambda_dssim 0 \
   -u 7000 \
-  --iterations 30000
+  --iterations 30000 --port 7010
  
 检查第二阶段效果
 export CUDA_VISIBLE_DEVICES=0
@@ -244,14 +244,14 @@ python render.py \
 -------------------------------------------------------------------------------------------------------------------------
 
 第一阶段训练 训练3D高斯 无形变
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/5m_with_base.zero \
   -m out_tdcr5_with_base \
   --joints 15 \
   --lambda_mask 2.0 \
   --opacity_reset_interval 100000000 \
-  -u 7000
+  -u 7000 --port 7011
 
 
 检查第一阶段效果
@@ -264,7 +264,7 @@ python render.py \
 cp out_tdcr5_with_base/chkpnt_7000.pth out_tdcr5_with_base/chkpnt_7000_stage1_backup.pth
 
 第二阶段 学习形变
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=5
 python train2.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/5m_with_base.all \
   -m out_tdcr5_with_base_stage2 \
@@ -273,7 +273,7 @@ python train2.py \
   --lambda_mask 2.0 \
   --lambda_dssim 0 \
   -u 7000 \
-  --iterations 30000
+  --iterations 30000 --port 7012
  
 检查第二阶段效果
 export CUDA_VISIBLE_DEVICES=0
