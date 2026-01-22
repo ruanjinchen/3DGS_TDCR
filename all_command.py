7b16c61f3bd46fd87a85ebc19e654638aa996272
ruanjinchen <ruanjinchen@foxmail.com>
2026-01-22 20:27:56 +0800

    最终全部训练指令

--- all_command.py
+++ all_command.py
@@ -281,4 +281,103 @@ python render.py \
   -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/5m_with_base.all \
   -m out_tdcr5_with_base_stage2 \
   --iteration 30000
+
+
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------REAL 2 with base--------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+
+第一阶段训练 训练3D高斯 无形变
+export CUDA_VISIBLE_DEVICES=3
+python train2.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_2m_with_base.zero \
+  -m out_real_tdcr2_with_base \
+  --joints 6 \
+  --lambda_mask 2.0 \
+  --opacity_reset_interval 100000000 \
+  -u 7000 --port 7003
+
+
+检查第一阶段效果
+export CUDA_VISIBLE_DEVICES=5
+python render.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_2m_with_base.zero \
+  -m out_real_tdcr2_with_base \
+  --iteration 7000
+  
+cp out_real_tdcr2_with_base/chkpnt_7000.pth out_real_tdcr2_with_base/chkpnt_7000_stage1_backup.pth
+
+第二阶段 学习形变
+export CUDA_VISIBLE_DEVICES=3
+python train2.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_2m_with_base.all \
+  -m out_real_tdcr2_with_base_stage2 \
+  -k out_real_tdcr2_with_base/chkpnt_7000_stage1_backup.pth \
+  --joints 6 \
+  --lambda_mask 2.0 \
+  --lambda_dssim 0 \
+  -u 7000 \
+  --iterations 30000 --port 7004
+ 
+检查第二阶段效果
+export CUDA_VISIBLE_DEVICES=5
+python render.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_2m_with_base.all \
+  -m out_real_tdcr2_with_base_stage2 \
+  --iteration 30000
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------REAL 3 with base--------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------
+
+第一阶段训练 训练3D高斯 无形变
+export CUDA_VISIBLE_DEVICES=0
+python train2.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_3m_with_base.zero \
+  -m out_real_tdcr3_with_base \
+  --joints 9 \
+  --lambda_mask 2.0 \
+  --opacity_reset_interval 100000000 \
+  -u 7000 --port 7007
+
+
+检查第一阶段效果
+export CUDA_VISIBLE_DEVICES=0
+python render.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_3m_with_base.zero \
+  -m out_real_tdcr3_with_base \
+  --iteration 7000
+  
+cp out_real_tdcr3_with_base/chkpnt_7000.pth out_real_tdcr3_with_base/chkpnt_7000_stage1_backup.pth
+
+第二阶段 学习形变
+export CUDA_VISIBLE_DEVICES=1
+python train2.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_3m_with_base.all \
+  -m out_real_tdcr3_with_base_stage2 \
+  -k out_real_tdcr3_with_base/chkpnt_7000_stage1_backup.pth \
+  --joints 9 \
+  --lambda_mask 2.0 \
+  --lambda_dssim 0 \
+  -u 7000 \
+  --iterations 30000 --port 7008
+ 
+检查第二阶段效果
+export CUDA_VISIBLE_DEVICES=0
+python render.py \
+  -s /data/yxk/K-data/K/fllm-sm/sim/3dgs/real_3m_with_base.all \
+  -m out_real_tdcr3_with_base_stage2 \
+  --iteration 30000
+
 '''
